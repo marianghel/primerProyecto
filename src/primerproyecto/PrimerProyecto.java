bf60004459aeaa42686c24b227e229ae7f18b9fd
Tata <Tata@10.68.172.207>
2015-03-13 14:15:42 -0600

    primera subida

--- src/primerproyecto/PrimerProyecto.java
+++ src/primerproyecto/PrimerProyecto.java
@@ -16,6 +16,7 @@ public class PrimerProyecto {
      */
     public static void main(String[] args) {
         System.out.println("prueba");
+        System.out.println("segunda prueba");
     }
     
 }
